e42de9d7b55d144ffc0504acfe634f86ba575480
Duy Pham <ericpham19@outlook.com>
2022-09-22 19:47:46 +1000

    finally finished

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,21 +4,21 @@ VALUES ("Lord of the rings", 1 , 2),
 
 
 INSERT INTO books ( title, year,  series_id, character_id)
-VALUES ("Fellowship of the rings", 1997, 1 ,2),
-    ("Fellowship of the rings", 1997, 1 ,2),
-    ("Fellowship of the rings", 1997, 1 ,2),
-    ("Fellowship of the rings", 1997, 1 ,2),
-    ("Fellowship of the rings", 1997, 1 ,2),
-    ("Fellowship of the rings", 1997, 1 ,2);
+VALUES ("Fellowship of the rings", 1997, 1 ,1),
+    ("Fellowship of the rings", 1997, 1 ,1),
+    ("Fellowship of the rings", 1998, 1 ,1),
+    ("Fellowship of the rings", 1999, 2 ,2),
+    ("Fellowship of the rings", 2000, 2 ,2),
+    ("Fellowship of the rings", 2001, 2 ,2);
 
 INSERT INTO characters (name,  motto, species, author_id)
 VALUES ("FREDO", "I am invisible", "dwarf", 1),
-        ("FREDO", "I am invisible", "dwarf", 1),
-        ("FREDO", "I am invisible", "dwarf", 1),
-        ("FREDO", "I am invisible", "dwarf", 1),
-        ("FREDO", "I am invisible", "dwarf", 1),
-        ("FREDO", "I am invisible", "dwarf", 1),
-        ("FREDO", "I am invisible", "dwarf", 1),
+        ("FREDO", "I am invisible", "human", 1),
+        ("FREDO", "I am invisible", "human", 1),
+        ("FREDO", "I am invisiblessssssssss", "dwarf", 2),
+        ("FREDO", "I am invisible", "dwarf", 2),
+        ("FREDO", "I am invisible", "dwarf", 2),
+        ("FREDO", "I am invisible", "dwarf", 2),
         ("FREDO", "I am invisible", "elf", 1);
 UPDATE characters SET species = "Martian" where species = "elf";
 
@@ -48,15 +48,10 @@ VALUES ("Tom", 1, 2),
         ("Tom", 1, 2),
         ("Tom", 1, 2),
         ("Tom", 1, 2);
-       
-        
--- Populate the database with the following:
+     
+ 
 
--- 2 series
--- 2 sub-genres
--- 2 authors
--- 3 books in each series
--- 8 characters
--- 4 characters in each series
--- of each of those 4, make 2 in all of the books in a series, and 2 in just 1 book in a series
--- Note you will need to insert values into your character_books join table
+
+
+
+
